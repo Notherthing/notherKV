87147eb18569b04846165377156c59c6788c2cf6
Nothering <1040986768@qq.com>
2022-09-21 10:26:06 +0800

    9.20

--- include/element/skiplist.h
+++ include/element/skiplist.h
@@ -1,4 +1,14 @@
-#ifdef __SKIPLIST_H__
+#ifndef __SKIPLIST_H__
 #define __SKIPLIST_H__
 
+#include <atomic>
+
+namespace notherkv {
+
+template <typename _KeyType, typename _KeyComparator, typename _Allocator>
+class Skiplist {
+    
+};
+
+}  // namespace notherkv
 #endif
